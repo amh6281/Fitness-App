599f00d289e38ad7b8fc7eb88009d4d45681b07a
amh6281 <amh6281@naver.com>
2023-01-17 12:51:29 +0900

    Navbar component 생성 2022.01.17

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import Navbar from "@/components/navbar";
+
 function App() {
-  return <div className="app">hello</div>;
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
