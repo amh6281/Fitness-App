622897d4ced2706e7ef651f9376392ec88853cba
amh6281 <amh6281@naver.com>
2023-01-18 19:22:58 +0900

    SelectedPage enum 정의 2022.01.18
    Navbar의 요소 중 SelectedPage를 enum으로 정의한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import Navbar from "@/components/navbar";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
 
   return (
     <div className="app bg-gray-20">
