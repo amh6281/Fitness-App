a0d7cfe12d978c2c272303a7c183d2b3292506f5
amh6281 <amh6281@naver.com>
2023-01-19 11:28:29 +0900

    ActionButton component 생성 2022.01.19
    버튼에 마우스 hover시 색상이 바뀌고, 해당 페이지로 이동하는 ActionButton 컴포넌트를 생성한다.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "../../assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "../actionButton/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -50,7 +51,9 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Register</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Register
+                  </ActionButton>
                 </div>
               </div>
             ) : (
