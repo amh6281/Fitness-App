2ef5d678d56a1be98e5e53e5da150072cb0a648d
amh6281 <amh6281@naver.com>
2023-01-20 15:39:59 +0900

    Mobile menu modal item 추가 및 navbar 고정 2022.01.20
    메뉴 모달창에 navbar 구성요소를 추가하고, 스크롤 시 navbar를 고정한다.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -9,16 +9,20 @@ import ActionButton from "../actionButton/ActionButton";
 type Props = {
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
+  isTopOfPage: boolean;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
   const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false);
   const flexBetween = "flex items-center justify-between";
   const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             {/* Left */}
@@ -76,6 +80,29 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <button onClick={() => setIsMenuToggle(!isMenuToggle)}>
               <XMarkIcon className="h-6 w-6 text-gray-400" />
             </button>
+          </div>
+          {/* Menu Items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefit"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Class"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
           </div>
         </div>
       )}
