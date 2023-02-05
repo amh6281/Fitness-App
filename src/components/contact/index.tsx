d2684f847d83f391a8b4ea6cee60dc8ba9989190
amh6281 <amh6281@naver.com>
2023-02-05 10:52:47 +0900

    Contact 컴포넌트 form 구현 2022.02.05

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -10,6 +10,19 @@ type Props = {
 };
 
 const Contact = ({ setSelectedPage }: Props) => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Contact)}>
@@ -35,6 +48,28 @@ const Contact = ({ setSelectedPage }: Props) => {
             officia numquam repudiandae.
           </p>
         </motion.div>
+
+        {/* Form And Image */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              method="POST"
+              action="https://formsubmit.co/your@email.com"
+            ></form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   );
