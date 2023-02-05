9a92ea3f6e24faf0a3d2903777530bad4f73e7cb
amh6281 <amh6281@naver.com>
2023-02-05 10:43:22 +0900

    Contact 컴포넌트 Header 구현 2022.02.05

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,12 +1,43 @@
-import { SelectedPage } from "@/shared/types";
 import React from "react";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import Text from "@/shared/Text";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Contact = ({ setSelectedPage }: Props) => {
-  return <div>Contact</div>;
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Contact)}>
+        {/* Header */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <Text>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </Text>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, ex
+            soluta? Facere quia perspiciatis quos adipisci a necessitatibus,
+            optio cumque itaque fugit doloremque odit consectetur iure iusto,
+            officia numquam repudiandae.
+          </p>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Contact;
